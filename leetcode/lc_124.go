4b9756759dac27d026728b0a120f7a3f1443b4f3
aparkinlot <202020761+aparkinlot@users.noreply.github.com>
2025-05-09 14:36:48 -0500

    lc 124

---
+++ leetcode/lc_124.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxPathSum(root *TreeNode) int {
+	res := math.MinInt
+	var propogate func(*TreeNode) int
+	//fmt.Println(root.Val)
+	propogate = func(rootDFS *TreeNode) int {
+		if rootDFS == nil {
+			return 0
+		}
+		lsum := max(0, propogate(rootDFS.Left))
+		rsum := max(0, propogate(rootDFS.Right))
+
+		res = max(res, rootDFS.Val+lsum+rsum)
+		//fmt.Println(res)
+		//fmt.Println(rootDFS.Val + max(lsum, rsum))
+		return rootDFS.Val + max(lsum, rsum)
+	}
+
+	dfs := propogate(root)
+	return max(dfs, res)
+}
+
+func main() {
+	root := &TreeNode{
+		Val: -10,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	res := maxPathSum(root)
+	fmt.Println("ANS:", res)
+	root = &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 11,
+				Left: &TreeNode{
+					Val: 7,
+				},
+				Right: &TreeNode{
+					Val: 2,
+				},
+			},
+		},
+		Right: &TreeNode{
+			Val: 8,
+			Left: &TreeNode{
+				Val: 13,
+			},
+			Right: &TreeNode{
+				Val: 4,
+				Right: &TreeNode{
+					Val: 1,
+				},
+			},
+		},
+	}
+	res = maxPathSum(root)
+	fmt.Println("ANS:", res)
+}
